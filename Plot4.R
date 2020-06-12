a790a6fd96ee8cf7b8dca5aea957b78d2373b1f7
VIKAS0804 <60432945+VIKAS0804@users.noreply.github.com>
2020-06-12 19:09:49 +0530

    Create Plot4.R

---
+++ Plot4.R
@@ -0,0 +1,13 @@
+Data<-read.table("household_power_consumption.txt",header = TRUE,sep=";",colClass="character")
+fData<-Data[Data$Date=="1/2/2007"| Data$Date=="2/2/2007",]
+timeVector<-strptime(paste(fData$Date, fData$Time),"%d/%m/%Y %H:%M:%S")
+png(filename = "plot4.png",width = 480, height = 480, units = "px",bg = "white")
+par(mfrow=c(2,2))
+plot(timeVector,as.numeric(fData$Global_active_power),xlab=NA,ylab="Global Active Power (kilowatts)",type="l")
+plot(timeVector,as.numeric(fData$Voltage),xlab="datetime",ylab="Voltage",type="l")
+plot(timeVector,as.numeric(fData$Sub_metering_1),xlab=NA,ylab="energy sub metering",type="l")
+legend("topright",c("sub metering 1", "sub metering2 ", "sub metering 3"), col = c("black", "blue", "red"), lty = 1)
+points(timeVector,as.numeric(fData$Sub_metering_2),type="l", col = "blue")
+points(timeVector,as.numeric(fData$Sub_metering_3),type="l", col = "red")
+plot(timeVector,as.numeric(fData$Global_reactive_power),xlab="datetime",ylab="Global_reactive_power",type="l")
+dev.off()
